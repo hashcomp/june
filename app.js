a63b52413852bbd97bb6b5bb01c771fb44ebb638
whsprr <109601462+whsprr@users.noreply.github.com>
2023-06-25 18:14:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,24 @@ const markovData = {};
 function speak(message) {
   if ('speechSynthesis' in window) {
     const utterance = new SpeechSynthesisUtterance(message);
+    const voices = speechSynthesis.getVoices();
+    
+    // Find a female voice
+    const femaleVoice = voices.find(voice => voice.lang === 'en-US' && voice.name === 'Karen');
+    
+    // Set the voice property to the female voice
+    utterance.voice = femaleVoice;
+    
     speechSynthesis.speak(utterance);
   }
 }
+
+
 function sendSMS(message) {
   const url = `sms:?&body=${encodeURIComponent(message)} sent with JuneAI`;
   window.location.href = url;
 }
+
 function searchNearby(query) {
   const encodedQuery = encodeURIComponent(query);
   const url = `https://www.google.com/maps/search/?api=1&query=${encodedQuery}`;
@@ -81,30 +92,35 @@ function generateResponse(userMessage) {
         }
         previousQuestion = userMessage;
         displayMessage(response, "bot");
+        speak(response);
       })
       .catch(error => {
-        response = "I'm sorry, I couldn't find any information on that.";
+        console.log("Error while fetching Wikipedia data:", error);
+        response = "Sorry, I couldn't find any information on that topic. 😔";
+        previousQuestion = userMessage;
+        displayMessage(response, "bot");
+        speak(response);
+      });
+    return;
+      } else if (userMessage.startsWith("define")) {
+    const word = userMessage.replace("define", "").trim();
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0 && data[0].meanings && data[0].meanings.length > 0) {
+          const definition = data[0].meanings[0].definitions[0].definition;
+          response = `The definition of ${word} is: ${definition}`;
+        } else {
+          response = `Sorry, I couldn't find the definition for ${word}.`;
+        }
+        displayMessage(response, "bot");
+      })
+      .catch(error => {
+        response = "I'm sorry, there was an error retrieving the definition.";
         displayMessage(response, "bot");
       });
     return;
-  } else if (userMessage.toLowerCase().includes("time")) {
-    const now = new Date();
-    const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    response = `The current time is ${time}`;
-  } else if (userMessage.toLowerCase().includes("i feel")) {
-    const emotion = userMessage.toLowerCase().split("i feel ")[1];
-    if (emotion.includes("happy") || emotion.includes("glad")) {
-      response = "That's great to hear! 😁";
-    } else if (emotion.includes("sad") || emotion.includes("unhappy")) {
-      response = "I'm sorry to hear that. Is there anything I can do to help?";
-    } else if (emotion.includes("angry") || emotion.includes("frustrated")) {
-      response = "I'm sorry to hear that you're feeling that way. Can you tell me more about what's bothering you?";
-    } else {
-      response = "Thank you for sharing your feelings with me.";
-    }
-  } else if (userMessage.toLowerCase().includes("gen")) {
-    response = generateSentence();
-  } else if (userMessage.includes("how old are you")) {
+   } else if (userMessage.includes("how old are you")) {
     response = "I don't have an age as I am an AI, but I was created in 2022.";
   } else if (userMessage.includes("where are you from")) {
     response = "I am a virtual assistant, so I don't have a physical location. But I am here to help you!";
@@ -130,219 +146,98 @@ function generateResponse(userMessage) {
     response = "Sure, here's a fun fact: The world's oldest known joke is a Sumerian proverb from 1900 BC that says, 'Something which has never occurred since time immemorial; a young woman did not fart in her husband's lap.'";
 } else if (userMessage.includes("can you sing")) {
     response = "I'm an AI assistant, so I don't have a singing voice. However, I can help you with information or engage in a conversation.";
-    } else if (userMessage.includes("where can I find")) {
-    response = "You can try searching on the internet or checking relevant websites or stores near your location.";
-} else if (userMessage.includes("tell me a story")) {
+    } else if (userMessage.includes("where can i find")) {
+    const query = userMessage.replace("where can i find", "").trim();
+    searchNearby(query);
+    response = `Here are the search results for "${query}" on Google Maps.`;
+  } else if (userMessage.includes("tell me a story")) {
     response = "Once upon a time, in a land far away, there was a brave knight who embarked on a quest to save the kingdom from an evil dragon...";
-} else if (userMessage.includes("how do I")) {
-    response = "To accomplish that, you can follow these steps: [provide step-by-step instructions]";
-} else if (userMessage.includes("recommend a book")) {
-    response = "What genre or type of book are you interested in? That would help me provide a more relevant recommendation.";
-} else if (userMessage.includes("movie recommendation")) {
-    response = "Sure! What genre or type of movie are you in the mood for?";
 } else if (userMessage.includes("tell me a riddle")) {
     response = "Here's a riddle for you: I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?";
     } else if (userMessage.includes("nevermind")) {
     response = "Oh Ok 🫢";
     } else if (userMessage.includes("huh")) {
     response = "Oh Ok 🫢";
-    } else if (userMessage.includes("what's the capital of")) {
-  const country = userMessage.split("capital of")[1].trim();
-  fetchCountryCapital(country)
-    .then(capital => {
-      if (capital) {
-        response = `The capital of ${country} is ${capital}.`;
-      } else {
-        response = "Sorry, I couldn't find the capital for that country.";
-      }
-      displayMessage(response, "bot");
-    });
-  return;
-  } else if (userMessage.startsWith("where is the nearest")) {
-  const query = userMessage.replace("where is the nearest", "").trim();
-  searchNearby(query);
-  return;
-  } else if (userMessage.startsWith("send")) {
-  const message = userMessage.replace("send", "").trim();
-  sendSMS(message);
-  return;
-  } else if (userMessage.includes("guess")) {
-    response = playNumberGuessingGame(userMessage);
-  } else if (userMessage.includes("say")) {
-    const messageToSay = userMessage.replace("say", "");
-    speak(messageToSay);
-    return;
-    } else if (userMessage.startsWith("define")) {
-    const word = userMessage.replace("define", "").trim();
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-      .then(response => response.json())
-      .then(data => {
-        if (data.length > 0 && data[0].meanings && data[0].meanings.length > 0) {
-          const definition = data[0].meanings[0].definitions[0].definition;
-          response = `The definition of ${word} is: ${definition}`;
+  } else if (userMessage.includes("capital of")) {
+    const country = userMessage.replace("capital of", "").trim();
+    fetchCountryCapital(country)
+      .then(capital => {
+        if (capital) {
+          response = `The capital of ${country} is ${capital}.`;
         } else {
-          response = `Sorry, I couldn't find the definition for ${word}.`;
+          response = "Sorry, I couldn't find the capital of that country. 😔";
         }
+        previousQuestion = userMessage;
         displayMessage(response, "bot");
-      })
-      .catch(error => {
-        response = "I'm sorry, there was an error retrieving the definition.";
-        displayMessage(response, "bot");
+        speak(response);
       });
     return;
+  } else if (userMessage.includes("nearest")) {
+    const query = userMessage.replace("nearest", "").trim();
+    searchNearby(query);
+    response = `Here are the search results for "${query}" on Google Maps.`;
+  } else if (userMessage.includes("send")) {
+    const message = userMessage.replace("send", "").trim();
+    sendSMS(message);
+    response = `Sending Message: "${message}"`;
+  } else if (userMessage.includes("random number")) {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    response = `The random number is ${randomNumber}.`;
+  } else if (userMessage.includes("guess") && randomNumber !== null) {
+    const guess = parseInt(userMessage.replace("guess", "").trim());
+    if (isNaN(guess)) {
+      response = "Please enter a valid number.";
+    } else if (guess < randomNumber) {
+      response = "Too low! Guess a higher number.";
+    } else if (guess > randomNumber) {
+      response = "Too high! Guess a lower number.";
+    } else {
+      response = "Congratulations! You guessed the correct number.";
+      randomNumber = null;
+    }
   } else {
-    response = "I'm sorry, I don't understand. Can you please rephrase your question?";
-  } 
+    // Eliza-like response
+    const elizaResponses = [
+      "Why do you say that?",
+      "How does that make you feel?",
+      "Tell me more about it.",
+      "What do you think about it?",
+      "I'm here to listen. Please go on.",
+      "Can you elaborate on that?",
+      "Why do you think that is?",
+      "It sounds interesting. Please continue.",
+    ];
+
+    const endsWithQuestionMark = userMessage.endsWith("?");
+
+    if (endsWithQuestionMark) {
+      response = "Why are you asking me this question?";
+    } else {
+      response = elizaResponses[Math.floor(Math.random() * elizaResponses.length)];
+    }
+  }
 
   previousQuestion = userMessage;
   displayMessage(response, "bot");
-}
-
-
-// Function to display a message with typing effect
-function displayMessageWithTyping(message, sender) {
-  const messageElement = document.createElement("div");
-  messageElement.classList.add(sender);
-
-  const messageTextElement = document.createElement("p");
-  messageElement.appendChild(messageTextElement);
-
-  if (sender === "user") {
-    messageTextElement.innerText = "User: " + message; // prepend "User: " to user messages
-  } else {
-    messageTextElement.innerText = "JuneAI: ";
-
-    // Create a span element for the typing effect
-    const typingEffectSpan = document.createElement("span");
-    messageTextElement.appendChild(typingEffectSpan);
-
-    // Start the typing effect
-    let i = 0;
-    const typingInterval = setInterval(() => {
-      if (i < message.length) {
-        typingEffectSpan.innerText += message.charAt(i);
-        i++;
-      } else {
-        clearInterval(typingInterval);
-      }
-    }, 50); // Adjust the typing speed by changing the interval (milliseconds)
-  }
-
-  chatContainer.appendChild(messageElement);
-  chatContainer.scrollTop = chatContainer.scrollHeight;
+  speak(response);
 }
 
 function displayMessage(message, sender) {
-  const messageElement = document.createElement("div");
-  messageElement.classList.add(sender);
-
-  if (sender === "user") {
-    messageElement.innerText = "User: " + message; // prepend "User: " to user messages
-  } else {
-    messageElement.innerText = "JuneAI: ";
-    chatContainer.appendChild(messageElement);
-
-    // Simulate typing effect
-    let index = 0;
-    const typingDelay = 50; // Adjust the typing speed by changing the delay (in milliseconds)
-
-    const typeCharacters = () => {
-      if (index < message.length) {
-        const character = message[index];
-        if (character === " ") {
-          messageElement.innerHTML += "&nbsp;"; // Display space as HTML non-breaking space
-        } else {
-          messageElement.innerText += character;
-        }
-        index++;
-        setTimeout(typeCharacters, typingDelay);
-      } else {
-        chatContainer.scrollTop = chatContainer.scrollHeight;
-      }
-    };
-
-    setTimeout(typeCharacters, typingDelay);
-  }
-
-  chatContainer.appendChild(messageElement);
+  const messageContainer = document.createElement("div");
+  messageContainer.classList.add("message", sender);
+  messageContainer.textContent = message;
+  chatContainer.appendChild(messageContainer);
   chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
-
-
-function trainMarkovChain(text) {
-  const words = text.toLowerCase().split(/\s+/);
-
-  for (let i = 0; i < words.length - 1; i++) {
-    const currentWord = words[i];
-    const nextWord = words[i + 1];
-
-    if (!markovData[currentWord]) {
-      markovData[currentWord] = [];
-    }
-
-    markovData[currentWord].push(nextWord);
-  }
-}
-
-function generateSentence() {
-  const words = Object.keys(markovData);
-  const startingWord = words[Math.floor(Math.random() * words.length)];
-
-  let sentence = startingWord;
-  let currentWord = startingWord;
-
-  while (markovData[currentWord]) {
-    const nextWords = markovData[currentWord];
-    const nextWord = nextWords[Math.floor(Math.random() * nextWords.length)];
-
-    sentence += " " + nextWord;
-    currentWord = nextWord;
-
-    if (sentence.length > 100) {
-      break;
-    }
-  }
-
-  return sentence;
-}
-
-function playNumberGuessingGame(userMessage) {
-  const number = parseInt(userMessage.split(" ")[1]);
-
-  if (isNaN(number)) {
-    return "Please provide a valid number.";
-  }
-
-  if (!randomNumber) {
-    randomNumber = Math.floor(Math.random() * 100) + 1;
-  }
-
-  if (number < randomNumber) {
-    return "Too low! Try guessing a higher number.";
-  } else if (number > randomNumber) {
-    return "Too high! Try guessing a lower number.";
-  } else {
-    const response = `Congratulations! You guessed the correct number (${randomNumber}). Let me know if you want to play again.`;
-    randomNumber = null;
-    return response;
-  }
-}
-
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+function handleFormSubmit(event) {
+  event.preventDefault();
   const userMessage = userInput.value;
-  displayMessage(userMessage, "user");
-  userInput.value = "";
-  generateResponse(userMessage);
-});
+  if (userMessage) {
+    displayMessage(userMessage, "user");
+    userInput.value = "";
+    generateResponse(userMessage);
+  }
+}
 
-// Training the Markov chain data
-fetch("file.txt")
-  .then(response => response.text())
-  .then(text => {
-    trainMarkovChain(text);
-  })
-  .catch(error => {
-    console.log("Error while fetching training data:", error);
-  });
+chatForm.addEventListener("submit", handleFormSubmit);
